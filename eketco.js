c121dfaca755f05c27595299ea66ed045e04550a
Ryanquei <ryanqueiros85@gmail.com>
2024-08-18 20:45:09 -0300

    adicionando arquivos js e css

---
+++ eketco.js
@@ -0,0 +1,62 @@
+
+createCanvas(700, 600);
+}function setup() {
+  createCanvas(400, 400);
+}
+
+function draw(green) {
+  background(220);     
+}
+function draw(green) {
+    background("green");
+    
+    rect(0, 10, 100, 150);
+}
+function draw() {
+    background("green");
+
+    fill("orange");
+    cicle(0, 10, 100, 150);
+}
+function draw() {
+    background("green");
+    
+    stroke("green") 
+    fill("orange") 
+    (0, 15, 120, 150)
+}
+function draw() {
+  background(220);
+  
+  stroke("green");
+  fill("orange");
+  rect(mouseX, mouseY, 200, 180);
+}
+function draw() {
+  background(320);
+  
+  stroke("green");
+  fill("orange");
+  
+  console.log(mouseIsPressed);
+  rect(mouseX, mouseY, 200, 180);
+}function setup() {
+  // cria uma tela de 400px de largura por 400px de altura
+  createCanvas(1000, 700);
+}
+function setup(circle) {
+  createCanvas(500, 600);
+  background("orange")
+}
+
+function draw(circle) {
+  stroke("green");
+  fill("purple");
+  
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
+
+
